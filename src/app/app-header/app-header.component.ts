f5430af7f11e358eb8babc575209a94ad1fc0d39
Taric Jain <taricjain@outlook.com>
2018-01-08 14:53:30 +0530

    added sidebar menu for mobile viewport

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-app-header',
@@ -8,4 +9,22 @@ import { Component, Input } from '@angular/core';
 export class AppHeaderComponent {
     @Input()
     title: string;
+
+    hideAway(): void {
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        });
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item');
+    }
 }
