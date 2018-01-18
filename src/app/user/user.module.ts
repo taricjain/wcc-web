80c9a831d87100315e09ddfb3d29baaf7bb8469d
Taric Jain <taricjain@outlook.com>
2018-01-18 21:32:16 +0530

    changed theme to auburn blue

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { AboutComponent } from './about/about.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { AppComponent } from '../../app/app.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ],
+  declarations: [
+    AboutComponent, 
+    AppHeaderComponent, 
+    DashboardComponent, 
+    LoginComponent, 
+    MenuComponent,
+    AppComponent
+  ]
+})
+export class UserModule { }
